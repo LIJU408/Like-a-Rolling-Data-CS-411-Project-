bc956898ad6207a1483424df4913d6a5e901c381
CloudProgrammer12138 <60324083+CloudProgrammer12138@users.noreply.github.com>
2020-07-20 23:32:01 -0500

    Add files via upload

---
+++ cs411/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+</head>
+<body>
+<form method="POST">
+    <label for="SBS">Search By Singer:</label>
+    <input type="text" name="singer">
+    <lable>&nbsp;</label>
+    <button>Search</button>
+    <br>
+    <br>
+    <label for="SBN">Search By Song Name:</label>
+    <input type="text" name="name">
+    <br>
+    <br>
+    <label for="SBY">Search By Release Year: From</label>
+    <input type="text" name="year1">
+    <lable>To</label>
+    <input type="text" name="year2">
+    <br>
+    <br>
+</form>
+<?php
+$singername = $_POST["singer"]??"";
+$songname = $_POST["name"]??"";
+$year1 = $_POST["year1"]??0;
+
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "", "cs411");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+if(empty($_POST["year2"])){
+    // Attempt select query execution
+    $sql = "SELECT * FROM song WHERE Singer LIKE '%$singername%' AND SongName LIKE '%$songname%' AND ReleaseDate>='$year1'";
+}else{
+    $year2 = $_POST["year2"];
+    $sql = "SELECT * FROM song WHERE Singer LIKE '%$singername%' AND SongName LIKE '%$songname%' AND ReleaseDate>='$year1' AND ReleaseDate<='$year2'";
+}
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>SongID</th>";
+                echo "<th>SongName</th>";
+                echo "<th>Singer</th>";
+                echo "<th>ReleaseDate</th>";
+            echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['SongID'] . "</td>";
+                echo "<td>" . $row['SongName'] . "</td>";
+                echo "<td>" . $row['Singer'] . "</td>";
+                echo "<td>" . $row['ReleaseDate'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+?>
+</body>
+</html>
