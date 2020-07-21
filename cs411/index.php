7392ab93c24a3dec9e179056444698fcded06132
CloudProgrammer12138 <60324083+CloudProgrammer12138@users.noreply.github.com>
2020-07-21 13:09:38 +0800

    Update index.php

--- cs411/index.php
+++ cs411/index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
 </head>
 <body>
 <form method="POST">
